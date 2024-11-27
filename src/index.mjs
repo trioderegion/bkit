2d396344d62ef8ab9015e1f5b5a1f7cde1ea14b5
trioderegion <matt.haentschke@gmail.com>
2024-11-27 16:01:20 -0500

    eh, shelving PM stuff for now.

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,55 @@
 import './styles/main.scss';
+import {detailsNodes, DetailsView} from '@mh4gf/prosemirror-details';
 
-Hooks.on('ready', () => console.log('FCCT Loaded') );
+Hooks.on("renderItemSheetV2", (app) => app.activateTab("activities", {triggerCallback: true}));
+Hooks.on('setup', () => {
+  console.log('FCCT Loaded');
+
+  //Object.assign(ProseMirror.defaultSchema, new ProseMirror.Schema({
+  //  nodes: {
+  //    ...ProseMirror.defaultSchema.nodes,
+  //    ...detailsNodes({ 
+  //      detailsGroup: 'block',
+  //      detailsContent: 'block+',
+  //      summaryContent: 'text*',
+  //    })
+  //  }
+  //}));
+  //ProseMirror.defaultPlugins.details = ProseMirrorDetails.build(ProseMirror.defaultSchema); 
+});
+
+class ProseMirrorDetails extends ProseMirror.ProseMirrorPlugin {
+
+  constructor(schema, options = {}) {
+ 
+    super(new ProseMirror.Schema({
+     nodes: {
+       ...schema.nodes,
+       ...detailsNodes({ 
+         detailsGroup: 'block',
+         detailsContent: 'block+',
+         summaryContent: 'text*',
+       })
+     }
+   }));
+
+  }
+
+  views() {
+    return {
+      details: (node, view, getPos) => new DetailsView(node, view, getPos)
+    }
+  }
+
+  static build(schema, options = {}) {
+    const plugin = new ProseMirrorDetails(schema, options);
+    return new ProseMirror.Plugin({
+      //nodeViews: plugin.views(),
+   //   view(eView) {
+    });
+  }
+}
+
+//Hooks.on('createProseMirrorEditor', (dataPath, plugins, editor) => {
+//  console.log('PM Plugins', plugins);
+//});
