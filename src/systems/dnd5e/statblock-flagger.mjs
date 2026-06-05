c8fd8a4de8c4e5034858672936ebd1af16f784f1
trioderegion <matt.haentschke@gmail.com>
2026-06-05 13:18:34 -0400

    removed some dead code

--- src/systems/dnd5e/statblock-flagger.mjs
+++ src/systems/dnd5e/statblock-flagger.mjs
@@ -37,25 +37,4 @@ class StatblockFlagger extends Flagger {
   static compatible(doc) {
     return super.compatible(doc) && (doc?.type === 'npc');
   }
-
-//  _reset() {
-//    const model = this.flagData.constructor;
-//
-//    /* Flag data is held directly on the package scope */
-//    if (model.inner.length == 0) {
-//      const keys = Object.keys(this.flagData.schema.fields);
-//      return Promise.all(keys.map(key => 
-//        this.document.unsetFlag(this.flagData.constructor.scope, key)
-//      ));
-//    }
-//
-//    /* flag data is a key within a package scope */
-//    if (model.inner.length == 1) {
-//      return this.document.unsetFlag(this.constructor.FLAG_MODEL.scope, this.constructor.FLAG_MODEL.inner.at(0));
-//    }
-//
-//    if (model.inner.length > 1) {
-//      return ui.notifications.error('Complex flag structure requires specific _reset implementation! No updates performed.');
-//    }
-//  }
 }
